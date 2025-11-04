06fc798fd809ab388e4f4f260597b326f87b23ec
Winterhana <haha920@naver.com>
2025-11-05 08:40:41 +0900

    init : README.md 파일 수정, 파일 구조 개선

--- Core_JavsScript/Chapter3_This/function_method_1.js
+++
@@ -1,9 +0,0 @@
-var func = function(x) {
-    console.log(this, x);
-};
-func(1); // Windows { ... } 1
-
-var obj = {
-    method: func
-};
-obj.method(2); // { method : f } 2
--- Core_JavsScript/index.js
+++
@@ -1,7 +0,0 @@
-console.log("test");
-
-const a = "study javascript";
-
-console.log(a);
-
-console.log("GitTest");
